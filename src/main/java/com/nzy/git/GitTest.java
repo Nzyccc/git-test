2c5c72404972effe1f1cf0a96667783d98991131
Nzy <1661461071@qq.com>
2022-05-31 19:12:37 +0800

    push commit

--- src/main/java/com/nzy/git/GitTest.java
+++ src/main/java/com/nzy/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello git fix");
         System.out.println("master change");
         System.out.println("hot-fix change");
+        System.out.println("push test");
     }
 }
