dfeebd12b989ffd303e6d8be75e7c3bc3c3cd219
Nzy <1661461071@qq.com>
2022-05-31 16:40:27 +0800

    first commit

---
+++ src/main/java/com/nzy/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.nzy.git;
+
+/**
+ * @author nzy
+ * @create 2022-05-31 下午4:31
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
